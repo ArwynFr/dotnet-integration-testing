b101b547dd4f158786ceb32ca969d7e6188d299d
ArwynFr <10365998+ArwynFr@users.noreply.github.com>
2024-02-02 18:28:59 +0100

    docs: try fix linters

--- src/ArwynFr.IntegrationTesting.Tests.Target/DummyDbContext.cs
+++ src/ArwynFr.IntegrationTesting.Tests.Target/DummyDbContext.cs
@@ -8,7 +8,7 @@ public class DummyDbContext : DbContext
 {
     public DummyDbContext(DbContextOptions options) : base(options) => Expression.Empty();
 
-    public DbSet<DummyEntity> Entities { get; init; }
+    public DbSet<DummyEntity> Entities => Set<DummyEntity>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
         => modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
