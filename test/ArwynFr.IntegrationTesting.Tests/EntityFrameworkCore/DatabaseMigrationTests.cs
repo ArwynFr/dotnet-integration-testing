1fe59856b77284c704b1b08c68100bf53659e844
Adrien Hupond <10365998+ArwynFr@users.noreply.github.com>
2024-04-28 22:19:14 +0200

    test: fix async void

--- test/ArwynFr.IntegrationTesting.Tests/EntityFrameworkCore/DatabaseMigrationTests.cs
+++ test/ArwynFr.IntegrationTesting.Tests/EntityFrameworkCore/DatabaseMigrationTests.cs
@@ -9,7 +9,7 @@ namespace ArwynFr.IntegrationTesting.Tests.EntityFrameworkCore;
 public class DatabaseMigrationTests(ITestOutputHelper output) : IntegrationTestBase<Program, MigrationDbContext>(output)
 {
     [Fact]
-    public async void TestShouldCreateDatabase()
+    public async Task TestShouldCreateDatabase()
     {
         var migrations = await Database.Database.GetPendingMigrationsAsync();
         migrations.Should().BeEmpty();
