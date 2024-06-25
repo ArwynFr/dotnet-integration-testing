90a067ee9ee583161bcb6e29ec0f7c2055a955df
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2024-06-25 10:51:13 +0200

    fix: support non-relational db
    Fix #47

---
+++ test/ArwynFr.IntegrationTesting.Tests/EntityFrameworkCore/DatabaseNonRelationalTests.cs
@@ -0,0 +1,22 @@
+﻿using ArwynFr.IntegrationTesting.Tests.Target;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ArwynFr.IntegrationTesting.Tests.EntityFrameworkCore;
+
+public class DatabaseNonRelationalTests(ITestOutputHelper output) : IntegrationTestBase<Program, MigrationDbContext>(output)
+{
+    [Fact]
+    public void TestShouldCreateDatabase()
+    {
+        Database.Database.IsInMemory().Should().BeTrue();
+    }
+
+    protected override IDatabaseTestStrategy<MigrationDbContext> DatabaseTestStrategy
+        => IDatabaseTestStrategy<MigrationDbContext>.DatabasePerTest;
+
+    protected override void ConfigureDbContext(DbContextOptionsBuilder builder)
+        => builder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+}
